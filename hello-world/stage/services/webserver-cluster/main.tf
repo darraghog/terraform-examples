ef3245388ed8cbfcc22f40573c1cb04b750f540a
Darragh O'Grady <darragh.ogrady@gmail.com>
2018-11-13 18:41:04 -0500

    chapter 3 state management

--- hello-world/stage/services/webserver-cluster/main.tf
+++ hello-world/stage/services/webserver-cluster/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 0.8, < 0.12"
+  backend "s3" {}
 }
 
 provider "aws" {
@@ -16,11 +17,7 @@ resource "aws_launch_configuration" "example" {
   image_id = "ami-40d28157"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
-  user_data = <<-EOF
-	#!/bin/bash
-	echo "Hello, World" > index.html
-	nohup busybox httpd -f -p "${var.server_port}" &
-	EOF
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle = {
 	create_before_destroy = true
@@ -58,6 +55,25 @@ resource "aws_security_group" "elb" {
 }
 
 data "aws_availability_zones" "all" {}
+
+data "terraform_remote_state" "db" {
+	backend = "s3"
+	config {
+		bucket="darraghog-terraform-up-and-running-state-us-east-1"
+		key="stage/services/data-stores/mysql/terraform.tfstate"
+		region="us-east-1"
+	}
+}
+
+data "template_file" "user_data" {
+	template = "${file("user-data.sh")}"
+
+	vars {
+		server_port = "${var.server_port}"
+		db_address = "${data.terraform_remote_state.db.address}"
+		db_port = "${data.terraform_remote_state.db.port}"
+	}
+}
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
